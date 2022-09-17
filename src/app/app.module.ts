5cc01bc9c1cc9e46cd3e66038df5d7a27535fef6
a844699 <papa-babacar-ngor.senghor@atos.net>
2022-09-17 20:32:03 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ServicesComponent } from './components/services/services.component';
 import { TechnosComponent } from './components/technos/technos.component';
@@ -11,18 +10,19 @@ import { FooterComponent } from './components/footer/footer.component';
 import { TeamComponent } from './components/team/team.component';
 import { ContactComponent } from './components/forms/contact/contact.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
     ServicesComponent,
     TechnosComponent,
     FooterComponent,
     TeamComponent,
     ContactComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
