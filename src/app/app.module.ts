8ca4935c7a5a4998c7ee161e06a33440eaed9fe0
a844699 <papa-babacar-ngor.senghor@atos.net>
2022-11-15 09:57:32 +0100

    new design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,15 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBsihXKp1XfK-_aBQon36bO6C40GbEhzFc",
+    authDomain: "coup2pouce-b7cb3.firebaseapp.com",
+    projectId: "coup2pouce-b7cb3",
+    storageBucket: "coup2pouce-b7cb3.appspot.com",
+    messagingSenderId: "333424420304",
+    appId: "1:333424420304:web:90f5b6f7be43e4bc3245d5",
+    measurementId: "G-SDSZHJ59FB"
+    }),
     AngularFirestoreModule,
     AngularFireAuthModule
 
